7e96a59bb90405cdef54470ba03b308976260016
lrsec <lrsecly@gmail.com>
2015-06-03 14:05:27 +0800

    增加 seelog 配置

--- log/log.go
+++ log/log.go
@@ -7,16 +7,41 @@ Also Adapte seelog to glog interface, in order to use seelog in grpc package.
 
 import (
 	"go/build"
+	"sync"
 
-	log "github.com/cihub/seelog"
+	log "linbox/seelog"
 )
 
 const (
-	DefSeelogCfgPath     string = "github.com/lrsec/l-grpc/config"
+	DefSeelogCfgPath     string = "linbox/config"
 	DefSeelogCfgFileName string = "seelog.xml"
 )
 
+var (
+	logConfigLocker sync.Mutex = sync.Mutex{}
+)
+
 func InitDefaultLogger() {
+	sync.Once.Do(readDefaultLogConfig)
+}
+
+func InitLoggerByConfig(fileName string) {
+	defer func() {
+		logConfigLocker.Unlock()
+	}()
+
+	logConfigLocker.Lock()
+
+	logger, err := log.LoggerFromConfigAsFile(fileName)
+	if err != nil {
+		log.Errorf("Can not create log from config file. FileName: %s", fileName)
+		return
+	}
+
+	log.ReplaceLogger(logger)
+}
+
+func readDefaultLogConfig() {
 	cfgPackage, err := build.Default.Import(DefSeelogCfgPath, "", build.FindOnly)
 	if err != nil {
 		log.Errorf("Can not find default log config package for zkkeeper. Package path: %s", DefSeelogCfgPath)
@@ -28,16 +53,6 @@ func InitDefaultLogger() {
 	logger, err := log.LoggerFromConfigAsFile(cfgName)
 	if err != nil {
 		log.Errorf("Can not create logger from default log config for zkkeeper. Log file full path: %s", cfgName)
-		return
-	}
-
-	log.ReplaceLogger(logger)
-}
-
-func InitLoggerByConfig(fileName string) {
-	logger, err := log.LoggerFromConfigAsFile(fileName)
-	if err != nil {
-		log.Errorf("Can not create log from config file. FileName: %s", fileName)
 		return
 	}
 
